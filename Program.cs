ee8b5a043a97ddda2bd95359515647dea3f8bd4e
AkmalDeveloper <rahmonovakmal2101@gmail.com>
2024-12-20 13:52:07 +0500

    dalete - namespace

--- Program.cs
+++ Program.cs
@@ -1,36 +1,30 @@
 ﻿using System;
 using OOP_Car;
 
-namespace OOP_Car
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Car car = new Car
-            (
-                model: "Mercedes benz W124 E500",
-                year: 1993,
-                price: 865_636_200m,
-                carSpeed: 250m
-            );
+        Car car = new Car
+        (
+            model: "Mercedes benz W124 E500",
+            year: 1993,
+            price: 865_636_200m,
+            carSpeed: 250m
+        );
 
-            Console.WriteLine(car.GetCarInfo());
+        Console.WriteLine(car.GetCarInfo());
 
-            int year = 2024;
-            decimal depreciatedPrice = car.CalculateDepreciation(year);
-            Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
+        int year = 2024;
+        decimal depreciatedPrice = car.CalculateDepreciation(year);
+        Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
 
-            year = 2000;
-            depreciatedPrice = car.CalculateDepreciation(year);
-            Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
-            
-            year = 1995;
-            depreciatedPrice = car.CalculateDepreciation(year);
-            Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
+        year = 2000;
+        depreciatedPrice = car.CalculateDepreciation(year);
+        Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
 
-
-
-        }
+        year = 1995;
+        depreciatedPrice = car.CalculateDepreciation(year);
+        Console.WriteLine($"{year} - yildagi narx : {depreciatedPrice} so'm");
     }
 }
